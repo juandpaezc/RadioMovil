a4598a0a41cff2589efa54fc66f01193b0d71bb5
Daniel Paez <juandanielpaezc@gmail.com>
2020-12-05 00:53:50 -0500

    Primer Boseto App RadioEnlace-- Calculo de distancias

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+
+  console.log("App executed");
   return (
+
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Ingrese</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Longitud 1"
+          style={styles.input}
+        />
+      </View>
+      <Text>Ingrese</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Latitud 1   "
+          style={styles.input}
+        />
+      </View>
+      <Text>Ingrese</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Longitud 2"
+          style={styles.input}
+        />
+      </View>
+      <Text>Ingrese</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Latitud 2   "
+          style={styles.input}
+        />
+      </View>
+      <Button title="Calcular" />
     </View>
   );
 }
@@ -17,5 +49,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 20,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    bottom: 20
   },
 });
